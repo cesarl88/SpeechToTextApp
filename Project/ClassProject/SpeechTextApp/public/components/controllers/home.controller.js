b597fe22df103bb04a90068d03f2d6feedc4b646
kbp46 <48021326+kbp46@users.noreply.github.com>
2019-03-18 23:39:21 -0400

    Add files via upload

--- Project/ClassProject/SpeechTextApp/public/components/controllers/home.controller.js
+++ Project/ClassProject/SpeechTextApp/public/components/controllers/home.controller.js
@@ -9,23 +9,43 @@
     function HomeController($location, $rootScope, $http) {
         var vm = this;
 
-        vm.home = Home;
+        vm.home = home;
+        console.log('In home')
+        vm.transcript = function ()
+        {
+            console.log('Hey going to transcript')
+            $location.path("/dashboard")   
+        }
+
+        vm.about = function ()
+        {
+            console.log('Hey going to about')
+            $location.path("/about")   
+        }
+
+        vm.profile = function ()
+        {
+            console.log('Hey going to profile')
+            $location.path("/profile")   
+        }
 
         function home() {
             vm.dataLoading = true;
 
-             $http.post('http://localhost:8000/account/home/', { username: vm.username, password: vm.password, first_name : vm.first_name, last_name: vm.last_name, email: vm.email})//, config)
-               .then(function (response) {
-                   console.log('Called home')
-                   console.log(response)
+            
+            
+            //  $http.post('http://localhost:8000/account/files/', { username: vm.username, password: vm.password, first_name : vm.first_name, last_name: vm.last_name, email: vm.email})//, config)
+            //    .then(function (response) {
+            //        console.log('Called home')
+            //        console.log(response)
                    
-                   //Store token DO NOT FORGET THIS PART
-                   $location.path('/home');
-                   //callback(response);
-               }, function (response) {
-                console.log("Error on home " + response)
-                //callback(response);
-            });
+            //        //Store token DO NOT FORGET THIS PART
+            //        $location.path('/home');
+            //        //callback(response);
+            //    }, function (response) {
+            //     console.log("Error on home " + response)
+            //     //callback(response);
+            // });
 
             // UserService.Create(vm.user)
             //     .then(function (response) {
